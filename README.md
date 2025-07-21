abc7cbc600efebb17292a550eb23554ffe1c2d48
Paige Weber <paigeweber31@gmail.com>
2025-07-20 19:53:04 -0500

    Added gallery,  added links to return to home,  password is now in envvar.

---
+++ README.md
@@ -0,0 +1,8 @@
+Be sure to set the upload password via:
+
+```
+export UPLOAD_PASSWORD='your_secret_password'
+```
+
+before starting the app!
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, abort
+from flask import Flask, request, render_template, redirect, abort, send_from_directory
 import os
 from werkzeug.utils import secure_filename
 
@@ -6,8 +6,8 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Shared password
-SHARED_PASSWORD = "party123"  # Change this to your secret password
+# Load shared password from environment variable
+SHARED_PASSWORD = os.environ.get("UPLOAD_PASSWORD", "changeme")
 
 @app.route("/", methods=["GET", "POST"])
 def upload():
@@ -23,8 +23,36 @@ def upload():
         for f in files:
             if f.filename:
                 f.save(os.path.join(user_dir, secure_filename(f.filename)))
-        return "Upload complete!"
+        return """
+  
+          <!DOCTYPE html>
+          <html>
+          <head><title>Upload Complete!</title></head>
+          <body>
+            <h1> Upload complete! </h1>
+            <br>
+            <a href="/">Upload Again</a>
+            <br>
+            <a href="/gallery">View Uploaded Photos</a>
+          </body>
+          </html>
+        """
     return render_template("upload.html")
+
+@app.route("/gallery")
+def gallery():
+    images = []
+    for user in os.listdir(UPLOAD_FOLDER):
+        user_dir = os.path.join(UPLOAD_FOLDER, user)
+        if os.path.isdir(user_dir):
+            for fname in os.listdir(user_dir):
+                images.append(f"/uploads/{user}/{fname}")
+    return render_template("gallery.html", images=images)
+
+@app.route("/uploads/<username>/<filename>")
+def uploaded_file(username, filename):
+    user_dir = os.path.join(UPLOAD_FOLDER, secure_filename(username))
+    return send_from_directory(user_dir, filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
